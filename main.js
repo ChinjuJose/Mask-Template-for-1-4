47a35ebfc93cedde80d00e13fdc40569843fa6d7
ChinjuJose <73712484+ChinjuJose@users.noreply.github.com>
2023-03-18 18:51:28 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup() {
+
+}
+
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+
+function draw() {
+
+}
+
+//Add parameters
+function gotResult() {
+  if (error) {
+    console.error(error);
+  } else {
+
+  }
+}
